986e1e2c6b6aacf9df46b1614726a6f3d2b7edb5
nirajan-fuse <nirajan.thakuri@fusemachines.com>
2026-02-02 11:16:20 +0545

    question 10 complete

--- src/q10.c
+++ src/q10.c
@@ -1,1 +1,21 @@
 // Write a C program to swap the values of two variables using a temporary variable.
+
+#include <stdio.h>
+
+int main() {
+    int x, y;
+    int temp;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+
+    printf(" Value of x before swap is %d and y is %d\n", x, y);
+
+    temp = x;
+    x = y;
+    y = temp;
+
+    printf(" Value of x after swap is %d and y is %d\n", x, y);
+
+    return 0;
+}
